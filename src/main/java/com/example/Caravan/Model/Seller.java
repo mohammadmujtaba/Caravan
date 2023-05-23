a0178f6d0257fe996e9718e6104bc0595f61e1a8
mohammadmujtaba <mohammadmujtaba262001@gmail.com>
2023-05-23 15:56:12 +0530

    Added Models

---
+++ src/main/java/com/example/Caravan/Model/Seller.java
@@ -0,0 +1,39 @@
+package com.example.Caravan.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "seller")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "mobile_no", unique = true, nullable = false)
+    String mobNo;
+
+    @Column(name = "email_id", unique = true, nullable = false)
+    String emailId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", nullable = false)
+    String gender;
+
+    @OneToMany(mappedBy = "seller", cascade = CascadeType.ALL)
+    List<Product> products = new ArrayList<>();
+}
